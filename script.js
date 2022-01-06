87a01b9fcc54b0e7efb56a6baf5fdf42e01649a1
vrushabhmungelwar <vrushabhmungelwar10@gmail.com>
2022-01-06 10:21:35 +0530

    added images

--- script.js
+++ script.js
@@ -5,16 +5,7 @@ fetch("https://moviesbyvrushabh.herokuapp.com/dogs")
 
 function initialize(data) {
   let x = Math.floor(Math.random() * 435 + 1);
-  console.log(x + "." + data[x].fact);
-
   document.querySelector(".modal-body").innerHTML += `<h5> ${
     x + ". " + data[x].fact
   }</h5>`;
 }
-
-var myModal = document.getElementById("myModal");
-var myInput = document.getElementById("myInput");
-
-myModal.addEventListener("shown.bs.modal", function () {
-  myInput.focus();
-});
